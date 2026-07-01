75e4b0c594c9b6f0adb4a081a35a380afeed15a5
Z-Hussein <z_h@gmx.de>
2026-07-01 20:50:43 +0200

    Bump version to 1.1.2

--- src/anywhereinput/__init__.py
+++ src/anywhereinput/__init__.py
@@ -5,6 +5,6 @@ Screen streaming, pixel-perfect touch mapping, keyboard input, and mouse control
 Zero-config tunnel support: Cloudflare, Tailscale, Pinggy, Zrok2, ngrok.
 """
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __author__ = "Z-Hussein"
 __license__ = "MIT"
